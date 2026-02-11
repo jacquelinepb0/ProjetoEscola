ba166344d520d6bb0602c13683cb55999320c50b
jacquelinepb0 <jac.pb14@gmail.com>
2026-02-11 10:00:12 -0300

    intervalo

--- script/index.js
+++ script/index.js
@@ -1,3 +1,26 @@
+let usuarios = [{
+    nome:"Jacqueline",
+    email:"jac.pb14@gmail.com"
+    senha:"1234"
+},
+usuarios = {
+    nome:"Lais",
+    email:"lais@gmail.com"
+    senha:"4567"
+},
+usuarios = {
+    nome:"Julia",
+    email:"julia@gmail.com"
+    senha:"89710"
+},
+usuarios = {
+    nome:"Carol",
+    email:"carol@gmail.com"
+    senha:"123"
+},
+]
+
+
 
 function login () {
     const email = document.getElementById("email").value
@@ -12,4 +35,17 @@ if (email ==="jac.pb14@gmail.com" && password === "1234") {
 } else {
     alert("Email ou senha incorretos")
 }
+}
+
+let usuarioValido = false
+for (let i = 0; i < usuarios.length; i++) {
+    if (email === usuarios[i].email && password === usuarios[i]) {
+        
+    } else {
+        
+    }
+    const element = array[i];
+    do {
+        
+    } while (condition);
 }
